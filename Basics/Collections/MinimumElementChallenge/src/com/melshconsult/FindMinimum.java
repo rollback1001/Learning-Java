019935def49c3e8ee4d934fec943a4b54f922551
rollback1001 <rollback1001@gmail.com>
2021-04-27 18:49:58 +0000

    Add logic for finding minimum value in an integer array

--- Basics/Collections/MinimumElementChallenge/src/com/melshconsult/FindMinimum.java
+++ Basics/Collections/MinimumElementChallenge/src/com/melshconsult/FindMinimum.java
@@ -1,4 +1,40 @@
 package com.melshconsult;
 
+import java.util.Scanner;
+
 public class FindMinimum {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static int[] readIntegers(int numIntegers) {
+        int[] userInputArray = new int[numIntegers];
+
+        System.out.println("Enter the integers");
+
+        for (int i = 0; i < numIntegers; i++) {
+            userInputArray[i] = scanner.nextInt();
+        }
+        return userInputArray;
+    }
+
+    public static int findMinimumInteger(int[] data) {
+        int minimumInteger = 0;
+        boolean flag = true;
+
+        while (flag) {
+            int temp;
+            flag = false;
+
+            // 10, 5, 60, 1, 2
+            for (int i = 0; i < data.length - 1; i++) {
+                if (data[i] < data[i + 1]) {
+                    temp = data[i];
+                    data[i+1] = temp;
+                    data[i] = data[i+1];
+                    flag = true;
+                }
+            }
+            minimumInteger = data[data.length-1];
+        }
+        return minimumInteger;
+    }
 }
--- Basics/Collections/MinimumElementChallenge/src/com/melshconsult/Main.java
+++ Basics/Collections/MinimumElementChallenge/src/com/melshconsult/Main.java
@@ -1,7 +1,18 @@
 package com.melshconsult;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter number of integers: ");
+        int numIntegers = scanner.nextInt();
+
+        int[] userInputArray = FindMinimum.readIntegers(numIntegers);
+
+        int minimumNumber = FindMinimum.findMinimumInteger(userInputArray);
+
+        System.out.printf("Minimum number is %d", minimumNumber);
     }
 }
