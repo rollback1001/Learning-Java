dc2d7ad81cf0bff435f29caa0459f27cd9e948e3
rollback1001 <rollback1001@gmail.com>
2021-04-28 03:01:54 +0000

    Add complete reverse array implementation

--- Basics/Collections/ReverseArrayChallenge/src/com/melshconsult/Main.java
+++ Basics/Collections/ReverseArrayChallenge/src/com/melshconsult/Main.java
@@ -2,6 +2,19 @@ package com.melshconsult;
 
 public class Main {
     public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7,8, 9, 10};
 
+        System.out.print("Original array elements: ");
+
+        for (int ele : array)
+            System.out.print(ele + " ");
+
+        System.out.println();
+
+        ReverseArray.reverse(array);
+        System.out.print("Reversed array elements: ");
+
+        for (int ele : array)
+            System.out.print(ele + " ");
     }
 }
--- Basics/Collections/ReverseArrayChallenge/src/com/melshconsult/ReverseArray.java
+++ Basics/Collections/ReverseArrayChallenge/src/com/melshconsult/ReverseArray.java
@@ -1,4 +1,14 @@
 package com.melshconsult;
 
 public class ReverseArray {
+    protected static void reverse(int[] arrayToReverse) {
+        int[] reverseArray = new int[arrayToReverse.length];
+        int counter = 0;
+
+        for (int i = arrayToReverse.length - 1; i >= 0; i--) {
+            reverseArray[counter] = arrayToReverse[i];
+            counter++;
+        }
+        System.arraycopy(reverseArray, 0, arrayToReverse, 0, reverseArray.length);
+    }
 }
