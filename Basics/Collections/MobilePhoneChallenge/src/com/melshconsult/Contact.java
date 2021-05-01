41fa1739e5113a5dd33b986992488d4ed9fc64a0
rollback1001 <rollback1001@gmail.com>
2021-05-01 05:48:30 +0000

    Add pojo implementation with only name and number fields

--- Basics/Collections/MobilePhoneChallenge/src/com/melshconsult/Contact.java
+++ Basics/Collections/MobilePhoneChallenge/src/com/melshconsult/Contact.java
@@ -1,4 +1,27 @@
 package com.melshconsult;
 
 public class Contact {
+	private String name;
+	private int number;
+
+	public Contact(String name, int number) {
+		this.name = name;
+		this.number = number;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
 }
