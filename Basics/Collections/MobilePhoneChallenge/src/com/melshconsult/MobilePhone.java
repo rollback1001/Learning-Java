b608483d0cd55ac2604d5aef5ac855d38b88e0c6
rollback1001 <rollback1001@gmail.com>
2021-05-01 06:51:17 +0000

    Add full impplement of store, search, modify and display contact functionalities

--- Basics/Collections/MobilePhoneChallenge/src/com/melshconsult/MobilePhone.java
+++ Basics/Collections/MobilePhoneChallenge/src/com/melshconsult/MobilePhone.java
@@ -1,4 +1,58 @@
 package com.melshconsult;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class MobilePhone {
+
+	private static Scanner scanner = new Scanner(System.in);
+
+	ArrayList<Contact> contacts;
+
+	public MobilePhone() {
+		contacts = new ArrayList<>();
+	}
+
+	protected void printContacts() {
+		for (Contact contact : contacts) {
+			System.out.println("Name: " + contact.getName() + " - " + "Number: " + contact.getNumber());
+		}
+	}
+
+	protected void storeContact(String name, int number) {
+		Contact newContact = new Contact(name, number);
+		contacts.add(newContact);
+	}
+
+	protected int searchContact(String name) {
+		for (Contact contact : contacts) {
+			if (contact.getName().equals(name)) {
+				return findContact(contact);
+			}
+		}
+		return -1;
+	}
+
+	protected void modifyContact(String searchName) {
+		if (searchContact(searchName) >= 0) {
+			System.out.print("Enter replacement name: ");
+			String newName = scanner.next().trim().toLowerCase();
+
+			System.out.print("Enter replacement number: ");
+			int newNumber = scanner.nextInt();
+
+			int currentContactPosition = searchContact(searchName);
+			Contact replacementContact = new Contact(newName, newNumber);
+
+			modifyContact(currentContactPosition, replacementContact);
+		}
+	}
+
+	private void modifyContact(int currentContactPosition, Contact replacementContact) {
+		contacts.set(currentContactPosition, replacementContact);
+	}
+
+	private int findContact(Contact searchContact) {
+		return contacts.indexOf(searchContact);
+	}
 }
